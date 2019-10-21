e4297c1a513c45c5325bbe275a3062c9621c371c
Sandeep Wali <swali86@gmail.com>
2019-10-20 21:17:39 -0700

    Minor styling tweaks on homepage.
    -Removed top box shadow.
    -Swapped confetti background image with sprinkles.
    -Made transition faster.
    -Changed positioning of slide components to be fixed margin from top rather than centered.

--- app/components/home/carousel.js
+++ app/components/home/carousel.js
@@ -2,18 +2,21 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { later } from '@ember/runloop';
 
-const numberOfSlides = 3;
+const NUMBER_OF_SLIDES = 3;
+const MILLISECONDS_PER_SLIDE = 5000;
 
 export default class Carousel extends Component {
   @tracked activeSlide = 0;
 
   constructor() {
     super(...arguments);
-    later(this.switchSlide.bind(this), 4000);
+    this.switchSlideDelayed();
   }
 
-  switchSlide() {
-    this.activeSlide = (this.activeSlide + 1) % numberOfSlides;
-    later(this.switchSlide.bind(this), 4000);
+  switchSlideDelayed() {
+    later(() => {
+      this.activeSlide = (this.activeSlide + 1) % NUMBER_OF_SLIDES;
+      this.switchSlideDelayed();
+    }, MILLISECONDS_PER_SLIDE);
   }
 }
