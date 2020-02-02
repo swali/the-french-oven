28b13fea1938ea580c1361922de8eef11e2aebff
Sandeep Wali <swali86@gmail.com>
2020-02-01 22:07:24 -0800

    Implemented cake grid

---
+++ app/components/cakes/everyday-cakes.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import cakes from 'the-french-oven/constants/cakes/everyday-cakes';
+
+export default Component.extend({
+  cakes,
+});
---
+++ app/constants/cakes/everyday-cakes.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    name: 'Sprinkly Vanilla',
+    description:
+      'Two 7" vanilla sponge filled with creamy vanilla icing. Decorated with 6 rosette and sprinkled with coated candy chocolate chips. Sides are finished with a vanilla cookie crunch.',
+    imageUrl: '/assets/images/cakes/7-inch-sprinky-vanilla.jpg',
+  },
+  {
+    name: 'Sprinkly Vanilla',
+    description:
+      'Two 7" vanilla sponge filled with creamy vanilla icing. Decorated with 6 rosette and sprinkled with coated candy chocolate chips. Sides are finished with a vanilla cookie crunch.',
+    imageUrl: '/assets/images/cakes/7-inch-sprinky-vanilla.jpg',
+  },
+  {
+    name: 'Sprinkly Vanilla',
+    description:
+      'Two 7" vanilla sponge filled with creamy vanilla icing. Decorated with 6 rosette and sprinkled with coated candy chocolate chips. Sides are finished with a vanilla cookie crunch.',
+    imageUrl: '/assets/images/cakes/7-inch-sprinky-vanilla.jpg',
+  },
+  {
+    name: 'Sprinkly Vanilla',
+    description:
+      'Two 7" vanilla sponge filled with creamy vanilla icing. Decorated with 6 rosette and sprinkled with coated candy chocolate chips. Sides are finished with a vanilla cookie crunch.',
+    imageUrl: '/assets/images/cakes/7-inch-sprinky-vanilla.jpg',
+  },
+  {
+    name: 'Sprinkly Vanilla',
+    description:
+      'Two 7" vanilla sponge filled with creamy vanilla icing. Decorated with 6 rosette and sprinkled with coated candy chocolate chips. Sides are finished with a vanilla cookie crunch.',
+    imageUrl: '/assets/images/cakes/7-inch-sprinky-vanilla.jpg',
+  },
+  {
+    name: 'Sprinkly Vanilla',
+    description:
+      'Two 7" vanilla sponge filled with creamy vanilla icing. Decorated with 6 rosette and sprinkled with coated candy chocolate chips. Sides are finished with a vanilla cookie crunch.',
+    imageUrl: '/assets/images/cakes/7-inch-sprinky-vanilla.jpg',
+  },
+  {
+    name: 'Sprinkly Vanilla',
+    description:
+      'Two 7" vanilla sponge filled with creamy vanilla icing. Decorated with 6 rosette and sprinkled with coated candy chocolate chips. Sides are finished with a vanilla cookie crunch.',
+    imageUrl: '/assets/images/cakes/7-inch-sprinky-vanilla.jpg',
+  },
+  {
+    name: 'Sprinkly Vanilla',
+    description:
+      'Two 7" vanilla sponge filled with creamy vanilla icing. Decorated with 6 rosette and sprinkled with coated candy chocolate chips. Sides are finished with a vanilla cookie crunch.',
+    imageUrl: '/assets/images/cakes/7-inch-sprinky-vanilla.jpg',
+  },
+];
