8b2def26254efd4e955363a3366166b9f5023507
Sandeep Wali <swali86@gmail.com>
2020-02-01 23:10:03 -0800

    Extracted cake-page component

--- app/components/cakes/everyday-cakes.js
+++
@@ -1,6 +0,0 @@
-import Component from '@ember/component';
-import cakes from 'the-french-oven/constants/cakes/everyday-cakes';
-
-export default Component.extend({
-  cakes,
-});
--- app/routes/everyday-cakes.js
+++ app/routes/everyday-cakes.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
+import cakes from 'the-french-oven/constants/cakes/everyday-cakes';
 
 export default Route.extend({
+  model() {
+    return { cakes };
+  },
 });
