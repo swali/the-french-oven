29a91621a9cd4c73c7530cf6c2f26ec33be92ed2
Rashna Razdan <rashnarazdan@gmail.com>
2020-02-03 00:12:32 -0800

    MODIFIED text in seasonal cakes

--- app/constants/cakes/seasonals-cakes.js
+++ app/constants/cakes/seasonals-cakes.js
@@ -2,31 +2,31 @@ export default [
   {
     name: 'Christmas Black Forest Cake',
     description:
-      'Chocolate sponge filled with cherry pastry filling, finished with non-dairy whipped topping and decorated with rosettes, candied cherries and chocolate shavings. Available in many sizes including quarter slab.',
+      'Chocolate sponge filled with cherry pastry filling, finished with non-dairy whipped topping and decorated with rosettes, candied cherries and chocolate shavings.',
     imageUrl: '/assets/images/cakes/seasonal_black_forest_xmas.jpg',
   },
   {
     name: 'Christmas Logs',
     description:
-      'Luxurious vanilla and chocolate sponge surrounded by choclate icing and decorated for Christmas. Available in many sizes and flavours.',
+      'Luxurious vanilla and chocolate sponge surrounded by choclate icing and decorated for Christmas.',
     imageUrl: '/assets/images/cakes/seasonal_yule_log.jpg',
   },
   {
     name: 'Christmas Cake',
     description:
-      'Two 7" vanilla cake layers filled with creamy vanilla icing and hand decorated for Christmas. Available in many sizes and flavours.',
+      'Two 7" vanilla cake layers filled with creamy vanilla icing and hand decorated for Christmas.',
     imageUrl: '/assets/images/cakes/seasonal_christmas_vanilla.jpg',
   },
   {
     name: 'Easter Cake',
     description:
-      'Moist vanilla cake with fluffy frosting and easter themed colors and decorations. Available in many flavours including chocolate.',
+      'Moist vanilla cake with fluffy frosting and easter themed colors and decorations.',
     imageUrl: '/assets/images/cakes/seasonal_vanilla_easter.jpg',
   },
   {
     name: 'Halloween Cake',
     description:
-      'Two layers of chocolate cake sorrounded by chocolate icing and decorated for halloween. Available in many sizes and flavours.',
+      'Two layers of chocolate cake sorrounded by chocolate icing and decorated for halloween.',
     imageUrl: '/assets/images/cakes/seasonal_halloween.jpg',
   },
   {
@@ -38,19 +38,19 @@ export default [
   {
     name: 'New Years Cake',
     description:
-      'Two 8" layers of choclate cake sorrounded by vanilla icing decorated with confetti and a plastic champagne bottle with Happy New Year sign. Available in many sizes and flavours.',
+      'Two 8" layers of choclate cake sorrounded by vanilla icing decorated with confetti and a plastic champagne bottle with Happy New Year sign.',
     imageUrl: '/assets/images/cakes/seasonal_new_year_chocolate.jpg',
   },
   {
     name: 'Harvest Cake',
     description:
-      'Two layers of vanilla cake sorrounded by vanilla icing and hand decorated for harvest. Available in many sizes and flavours.',
+      'Two layers of vanilla cake sorrounded by vanilla icing and hand decorated for harvest.',
     imageUrl: '/assets/images/cakes/seasonal_vanilla_harvest.jpg',
   },
   {
     name: 'Valentines Cake',
     description:
-      'Heart shaped vanilla cake with lovingly crafted hand decorations using creamy icing. Available in many sizes and flavours.',
+      'Heart shaped vanilla cake with lovingly crafted hand decorations using creamy icing.',
     imageUrl: '/assets/images/cakes/seasonal_vanilla_valentines.jpg',
   },
 ];
