5eb585eab0c49524f93fd1fe50804101f929cbff
Rashna Razdan <rrazdan@rrazdan-mn1.linkedin.biz>
2020-02-02 17:57:07 -0800

    added content for everyday cakesies

--- app/constants/cakes/everyday-cakes.js
+++ app/constants/cakes/everyday-cakes.js
@@ -1,50 +1,50 @@
 export default [
   {
-    name: 'Sprinkly Vanilla',
+    name: 'Sprinkly Vanilla Cake',
     description:
-      'Two 7" vanilla sponge filled with creamy vanilla icing. Decorated with 6 rosette and sprinkled with coated candy chocolate chips. Sides are finished with a vanilla cookie crunch.',
+      'Vanilla sponges filled with creamy vanilla icing, decorated with rosettes and sprinkled with chocolate chips. Also available in a chocolate flavour.',
     imageUrl: '/assets/images/cakes/7-inch-sprinky-vanilla.jpg',
   },
   {
-    name: 'Sprinkly Vanilla',
+    name: 'Double Chocolate Cake',
     description:
-      'Two 7" vanilla sponge filled with creamy vanilla icing. Decorated with 6 rosette and sprinkled with coated candy chocolate chips. Sides are finished with a vanilla cookie crunch.',
-    imageUrl: '/assets/images/cakes/7-inch-sprinky-vanilla.jpg',
+      'Chocolate sponges filled with creamy chocolate icing, decorated with rosettes and sprinkled with chocolate cookie crunch.',
+    imageUrl: '/assets/images/cakes/7-inch-double-chocoloate.jpg',
   },
   {
-    name: 'Sprinkly Vanilla',
+    name: 'Black Forest Cake',
     description:
-      'Two 7" vanilla sponge filled with creamy vanilla icing. Decorated with 6 rosette and sprinkled with coated candy chocolate chips. Sides are finished with a vanilla cookie crunch.',
-    imageUrl: '/assets/images/cakes/7-inch-sprinky-vanilla.jpg',
+      'Two chocolate sponges with a cherry pastry filling, finished with non-dairy whipped topping and decorated with rosettes, candied cherries and chocolate shavings.',
+    imageUrl: '/assets/images/cakes/7_inch_black_forest.jpg',
   },
   {
-    name: 'Sprinkly Vanilla',
+    name: 'Celebration Cake',
     description:
-      'Two 7" vanilla sponge filled with creamy vanilla icing. Decorated with 6 rosette and sprinkled with coated candy chocolate chips. Sides are finished with a vanilla cookie crunch.',
-    imageUrl: '/assets/images/cakes/7-inch-sprinky-vanilla.jpg',
+      'Chocolate cake baked with colorful sugar confetti, frosted with our signature colorful balloons. Also available in a vanilla flavour.',
+    imageUrl: '/assets/images/cakes/7-inch-celebration-chocoloate.jpg',
   },
   {
-    name: 'Sprinkly Vanilla',
+    name: 'Carrot Cake',
     description:
-      'Two 7" vanilla sponge filled with creamy vanilla icing. Decorated with 6 rosette and sprinkled with coated candy chocolate chips. Sides are finished with a vanilla cookie crunch.',
-    imageUrl: '/assets/images/cakes/7-inch-sprinky-vanilla.jpg',
+      'Two rounds of carrot cake filled with cream cheese icing, finished with traditional cream cheese icing and decorated with icing carrots.',
+    imageUrl: '/assets/images/cakes/7-inch-carrot.jpg',
   },
   {
-    name: 'Sprinkly Vanilla',
+    name: 'Chocolate Fudge Cake',
     description:
-      'Two 7" vanilla sponge filled with creamy vanilla icing. Decorated with 6 rosette and sprinkled with coated candy chocolate chips. Sides are finished with a vanilla cookie crunch.',
-    imageUrl: '/assets/images/cakes/7-inch-sprinky-vanilla.jpg',
+      'Two chocolate sponges filled and finished with creamy chocolate fudge icing, topped with a chocolate rosette.',
+    imageUrl: '/assets/images/cakes/7_inch_chocolate_fudge.jpg',
   },
   {
-    name: 'Sprinkly Vanilla',
+    name: 'Classic Caramel Cake',
     description:
-      'Two 7" vanilla sponge filled with creamy vanilla icing. Decorated with 6 rosette and sprinkled with coated candy chocolate chips. Sides are finished with a vanilla cookie crunch.',
-    imageUrl: '/assets/images/cakes/7-inch-sprinky-vanilla.jpg',
+      'Moist vanilla cake topped with rich melt-in-your-mouth caramel, decorated with vanilla shavings and finished with an elegant chocolate garnish.',
+    imageUrl: '/assets/images/cakes/7_inch_caramel.jpg',
   },
   {
-    name: 'Sprinkly Vanilla',
+    name: 'Mocca Cake',
     description:
-      'Two 7" vanilla sponge filled with creamy vanilla icing. Decorated with 6 rosette and sprinkled with coated candy chocolate chips. Sides are finished with a vanilla cookie crunch.',
-    imageUrl: '/assets/images/cakes/7-inch-sprinky-vanilla.jpg',
+      'Layer of creamy vanilla sponge filled with coffee flavoured chocolate fudge icing, finished with vanilla and chocolate cookie crumbs.',
+    imageUrl: '/assets/images/cakes/7_inch_mocca.jpg',
   },
 ];
