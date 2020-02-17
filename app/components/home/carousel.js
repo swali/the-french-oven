ff0a4ed9d1fa1c311a9d3594f0eed5958d5110c6
Sandeep Wali <swali86@gmail.com>
2020-02-16 16:57:47 -0800

    Lazy loading for carousel images on homepage

--- app/components/home/carousel.js
+++ app/components/home/carousel.js
@@ -1,7 +1,8 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { later } from '@ember/runloop';
-import { action } from '@ember/object';
+import { action, set } from '@ember/object';
+import CarouselCakes from 'the-french-oven/constants/home/carousel-cakes';
 
 const NUMBER_OF_SLIDES = 3;
 const MILLISECONDS_PER_SLIDE = 5000;
@@ -10,8 +11,38 @@ export default class Carousel extends Component {
   @tracked activeSlide = 0;
   autoSwitchSlides = true;
 
+  cakes = null;
+  initialLoadTime;
+
   constructor() {
     super(...arguments);
+    this.initCakes();
+  }
+
+  /**
+   * Only set the src for the first image. The remaining images will be lazy loaded.
+   */
+  initCakes() {
+    this.initialLoadTime = Date.now();
+    this.cakes = CarouselCakes.map((cake) => ({
+      name: cake.name,
+      imageUrl: '',
+    }));
+
+    this.cakes[0].imageUrl = CarouselCakes[0].imageUrl;
+
+    window.addEventListener('load', this.loadRemainingImages.bind(this));
+  }
+
+  /**
+   * After the first image is loaded, set the src for the other image and start the timer
+   * for slider.
+   */
+  loadRemainingImages() {
+    this.cakes.forEach((cake, index) => {
+      set(cake, 'imageUrl', CarouselCakes[index].imageUrl);
+    });
+
     this.switchSlideDelayed();
   }
 
---
+++ app/constants/home/carousel-cakes.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    name: 'Black Forest cake',
+    imageUrl: '/assets/images/home/slide1.jpg',
+  },
+  {
+    name: 'Confetti cake',
+    imageUrl: '/assets/images/home/slide2.jpg',
+  },
+  {
+    name: 'Cherry cake',
+    imageUrl: '/assets/images/home/slide3.jpg',
+  },
+];
