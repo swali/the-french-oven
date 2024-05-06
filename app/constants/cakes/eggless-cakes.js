b43cc7d9753acbd44a9572626c3776a7e5b5086a
Sandeep Wali <swali86@gmail.com>
2024-05-05 19:09:35 -0700

    Updated eggless cakes descriptions

--- app/constants/cakes/eggless-cakes.js
+++ app/constants/cakes/eggless-cakes.js
@@ -12,27 +12,21 @@ export default [
     imageUrl: '/assets/images/cakes/7_inch_strawberry.jpg',
   },
   {
-    name: 'Happy Birthday Chocolate Cake',
-    description:
-      'Two 7" chocolate sponges filled with chocolate icing. Tops and sides are finished with chocolate icing and decorated with sugar roses and leaves.',
-    imageUrl: '/assets/images/cakes/7_inch_happybirthday.jpg',
-  },
-  {
-    name: '8" Chocolate',
-    description:
-      'Chocolate sponge finished with chocolate fudge icing and decorated with a colorful rainbow. Sides are finished with chocolate fudge trim.',
-    imageUrl: '/assets/images/cakes/8_inch_chocolate.jpg',
-  },
-  {
-    name: 'Chocolate or Vanilla Quarter Slab',
-    description:
-      'Two layers of cake are sandwiched together and surrounded by icing, then decorated with rosebuds. Trims available in pink, blue, yellow and purple.',
-    imageUrl: '/assets/images/cakes/chocolate-all-occasion.jpg',
-  },
-  {
     name: 'Black Forest Quarter Slab',
     description:
       '10” x 7” chocolate sponge filled with cherry pastry filling, decorated with rosettes, candied cherries, chocolate cookie crunch and shavings.',
+    imageUrl: '/assets/images/cakes/eggless_black_forest_qrtr_slab.jpg',
+  },
+  {
+    name: 'Mango Cake',
+    description:
+      'Two layers of vanilla cake with mango crème filling decorated with a layer of mago filling on top and side of cake finished with chocoloate curls on side and chocolate drizzle on top of cake.',
+    imageUrl: '/assets/images/cakes/eggless_black_forest_qrtr_slab.jpg',
+  },
+  {
+    name: 'Sprinkle Vanilla Cake',
+    description:
+      'Two layers of eggless vanilla cake with vanilla icing and sprinkles.',
     imageUrl: '/assets/images/cakes/eggless_black_forest_qrtr_slab.jpg',
   },
 ];
