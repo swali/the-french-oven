0681aa5e5d82e584ae538c89bf851ba797584908
Sandeep Wali <swali86@gmail.com>
2024-06-06 20:19:50 -0700

    updating celeration cakes order and descriptions

--- app/constants/cakes/celebration-cakes.js
+++ app/constants/cakes/celebration-cakes.js
@@ -1,14 +1,32 @@
 export default [
   {
+    name: 'Christmas Logs',
+    description:
+      'Luxurious vanilla and chocolate sponge surrounded by choclate icing and decorated for Christmas.',
+    imageUrl: '/assets/images/cakes/black-forest-log.png',
+  },
+  {
+    name: 'Strawberry Bar Cake',
+    description:
+      'Three layers of moist vanilla sponge with vanilla crème and strawberry filling finished with chocolate decorations on top of cake.',
+    imageUrl: '/assets/images/cakes/strawberry-bar.jpg',
+  },
+  {
+    name: 'Chocolate Bar Cake',
+    description:
+      'Three layers of chocolate sponge with chocolate crème and caramel filling finished with chocolate ganache and decorated with caramel icing drizzle on top of cake.',
+    imageUrl: '/assets/images/cakes/chocolate-bar.jpg',
+  },
+  {
     name: 'Vanilla Cake',
     description:
-      '1/2 slab sponge finished with non-dairy vanilla cream, then hand decorated with large trim.',
+      'Two layers of vanilla cake with vanilla icing finished with top and bottom trim and decorations.',
     imageUrl: '/assets/images/cakes/celebration_confetti_vanilla.jpg',
   },
   {
     name: 'Chocolate Cake',
     description:
-      '1/2 slab sponge finished with non-dairy chocolate cream, then hand decorated with large trim.',
+      'Two layers of moist chocolate cake with chocolate icing finished with top and bottom trim and decorations.',
     imageUrl: '/assets/images/cakes/celebration_confetti_chocolate.jpg',
   },
   {
@@ -35,23 +53,4 @@ export default [
       'Two layers of carrot cake filled with delicious cream cheese icing. Covered with cream cheese icing, biscrunch on the sides and hand decorated with trim and icing carrots.',
     imageUrl: '/assets/images/cakes/celebration_carrot_half_slab.jpg',
   },
-  {
-    name: 'Christmas Logs',
-    description:
-      'Luxurious vanilla and chocolate sponge surrounded by choclate icing and decorated for Christmas.',
-    imageUrl: '/assets/images/cakes/black-forest-log.png',
-  },
-  {
-    name: 'Strawberry Bar Cake',
-    description:
-      'Three layers of moist vanilla sponge with vanilla crème and strawberry filling finished with chocolate decorations on top of cake.',
-    imageUrl: '/assets/images/cakes/strawberry-bar.jpg',
-  },
-  {
-    name: 'Chocolate Bar Cake',
-    description:
-      'Three layers of chocolate sponge with chocolate crème and caramel filling finished with chocolate ganache and decorated with caramel icing drizzle on top of cake.',
-    imageUrl: '/assets/images/cakes/chocolate-bar.jpg',
-  },
-
 ];
